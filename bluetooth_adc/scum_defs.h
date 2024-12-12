3bf9373470f44e05596efafdac3c63bde04700dc
Jacob <72148836+JacobLouie@users.noreply.github.com>
2024-12-13 07:39:59 +0800

    SCuM listening on 2.402GHz: scum_defs.h

--- bluetooth_adc/scum_defs.h
+++ bluetooth_adc/scum_defs.h
@@ -9,7 +9,7 @@
 
 // LC_code used to the initial LC frequency, before optical calibration
 #define DEFUALT_INIT_LC_CODE 740//board#2:160//813//625
-#define REFERENCE_LC_TARGET 250150//250138//249948//252522
+#define REFERENCE_LC_TARGET 250330//(2.404GHz)//250150//(2.402GHz)
 
 //=========================== typedef =========================================
 
