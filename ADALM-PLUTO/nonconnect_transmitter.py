aa088d5a76d87286d112b220b2c3ec6f5315f18c
Jacob <72148836+JacobLouie@users.noreply.github.com>
2025-02-14 04:36:12 +0800

    nonconnect_transmitter.py I see packet on my phone now
    use channel 37

--- ADALM-PLUTO/nonconnect_transmitter.py
+++ ADALM-PLUTO/nonconnect_transmitter.py
@@ -4,30 +4,28 @@ from Modules.progressbar import printProgressBar
 from Modules.ble_hardware import AD2Transmitter, PlutoTransmitter
 
 if __name__ == "__main__":
-    freqs = {37: 2.402e09, 38: 2.426e09, 39: 2.480e09}
-    symbol_time = 1.0e-6
+    freqs = {0:2.405e09, 37: 2.402e09, 38: 2.426e09, 39: 2.480e09} # 0 changed to 2.405 for SCuM test only
+    #symbol_time = 0.5e-6 #802.15.4
+    symbol_time = 1e-6 #BLE
     bt = 0.5
     tx_power = -50
-    ifreq = 2.5e6 #1.25e6
+    ifreq = 2.5e6
 
-    channels = [37, 38, 39]
-
-    packet = hex2bin(whiten_fullPacket(packet_gen('8e89bed6', 'ADV_NONCONN_IND', '90d7ebb19299', [['FLAGS', '06'], ['COMPLETE_LOCAL_NAME', 'SCUM3']]), 37))
-    rawPDU = packet_decode(packet, 37)
+    # channels = [0, 37, 38, 39]
+    channels = [37]
+    #packet = hex2bin(whiten_fullPacket(packet_gen('8e89bed6', 'ADV_NONCONN_IND', '90d7ebb19299', [['FLAGS', '06'], ['COMPLETE_LOCAL_NAME', 'SCUM3']]), 37))
+    #rawPDU = packet_decode(packet, 37)
 
     #print(f"Raw PDU: 0x{rawPDU}")
+   
     
-    #if input("Use AD2 or Pluto? (a/p) ").lower() == 'a':
-    #    sdr = AD2Transmitter(freqs[37], symbol_time, bt, tx_power)
-    #else:
-    #    sdr = PlutoTransmitter(freqs[37], symbol_time, bt, tx_power, ifreq)
-    sdr = PlutoTransmitter(freqs[37], symbol_time, bt, tx_power, ifreq)
+    if input("Use AD2 or Pluto? (a/p) ").lower() == 'a':
+        sdr = AD2Transmitter(freqs[37], symbol_time, bt, tx_power)
+    else:
+        sdr = PlutoTransmitter(freqs[37], symbol_time, bt, tx_power, ifreq)
+        sdr.set_sample_rate()
 
-    #packet = hex2bin('AAAAAAAAAAAAAAA0F0F0F0F0FFF0F0F0F0F0F0AAAAAAAAAAAAAAA')
-    #packet = hex2bin('FFFFFFF0F0F0F0F0F0F0F0F0F0F0F0FFF0F0F0F0F0F0FFFFFFFFF')
-    packet = hex2bin('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
-    #packet = hex2bin('F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F0F')
-    #packet = hex2bin('f0f01556b7d9171f14373cc31328d04ee0c2872f924dd6dd05b437ef6')
+    packet = hex2bin('1556b7d9171f14373cc31328d04ee0c2872f924dd6dd05b437ef6') # Packet for SCuM test
     sdr.set_packet(packet*100)
 
     while True:
@@ -38,7 +36,7 @@ if __name__ == "__main__":
             break
         
         for i in range(amt):
-            #printProgressBar(i + 1, amt, prefix="Progress:", suffix="Complete", length=50)
+            printProgressBar(i + 1, amt, prefix="Progress:", suffix="Complete", length=50)
             for ch in channels:
                 sdr.set_tx_freq(freqs[ch])
                 sdr.transmit(1,0)
