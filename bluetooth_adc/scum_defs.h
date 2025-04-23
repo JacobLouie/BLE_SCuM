4a1346ca4594412ec7764afebd18d656cdf67b52
Jacob <72148836+JacobLouie@users.noreply.github.com>
2025-04-23 11:55:22 +0800

    More LC targets for testing

--- bluetooth_adc/scum_defs.h
+++ bluetooth_adc/scum_defs.h
@@ -8,8 +8,10 @@
 //=========================== define ==========================================
 
 // LC_code used to the initial LC frequency, before optical calibration
-#define DEFUALT_INIT_LC_CODE 290//740//board#2:160//813//625
-#define REFERENCE_LC_TARGET 250200//(2.405GHz - 2500 kHz)
+#define DEFUALT_INIT_LC_CODE 180//2.405GHz
+#define REFERENCE_LC_TARGET 250180//(2.405GHz - 2.5 MHz = 2.4025GHz)
+//#define DEFUALT_INIT_LC_CODE 130//2.402GHz
+//#define REFERENCE_LC_TARGET 249870//(2.402Hz - 2.5 MHz = 2.3995GHz)
 
 //=========================== typedef =========================================
 
